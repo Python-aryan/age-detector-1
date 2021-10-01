47cae09c3514b074469870ceed9678c492ecfa33
Aryan Sonone <72177377+Python-aryan@users.noreply.github.com>
2021-10-01 06:02:16 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -51,7 +51,7 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     return JSONResponse({'result': int(round(float(learn.predict(img)[0][0]),0))})
-
+#This is the main file than acts a a server
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=5042)
 
